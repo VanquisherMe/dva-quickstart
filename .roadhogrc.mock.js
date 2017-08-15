8925cafa39eca4f412af03afec2fceae2f153d27
vanquisher <236514977@qq.com>
2017-08-15 20:04:30 +0800

    i

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,3 +1,6 @@
-
-export default {
-};
+const mock = {}
+//循环 mock 下的文件 导入请求拦截
+require('fs').readdirSync(require('path').join(__dirname + '/src/mock')).forEach(function(file) {
+  Object.assign(mock, require('./src/mock/' + file))
+})
+module.exports = mock
